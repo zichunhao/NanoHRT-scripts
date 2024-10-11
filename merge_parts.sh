5b5b737da4fa40e540b7228521c57bc5d15a965d
zichunhao <zichunh2000@gmail.com>
2024-10-11 08:48:31 +0200

    Use the tmp dir

--- merge_parts.sh
+++ merge_parts.sh
@@ -1,21 +1,23 @@
 era="2022"; 
 pattern="WtoLNu-2Jets_0J"; 
 path="/eos/user/z/zichun/higgs/HH4b-calib/HRT_ParT/GloParTStage2_20241009_ak8_qcd_${era}/mc/pieces"; 
+output_path="/eos/user/z/zichun/higgs/HH4b-calib/HRT_ParT/GloParTStage2_20241009_ak8_qcd_${era}/mc/outputs";
+mkdir -p ${output_path};
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p ${path}/${pattern}_more; 
 
-step=$1;
+TMP_DIR=/tmp/$(whoami)/nanoHRT/${pattern};
+mkdir -p ${TMP_DIR};
+cd ${TMP_DIR};
+echo "Moving"
+mv ${path}/${pattern}_*_tree.root ${path}/${pattern}_more; 
 
-if [ $step -eq 1 ]; then
-    echo "Moving"
-    mv ${path}/${pattern}_*_tree.root ${path}/${pattern}_more; 
-elif [ $step -eq 2 ]; then 
-    set -xe;
-    for i in {0..9}; do 
-        python3 ${SCRIPT_DIR}/haddnano.py ${path}/${pattern}_${i}_tree.root ${path}/${pattern}_more/${pattern}_*${i}_tree.root; 
-    done
-else
-    echo "step is either 1 or 2"
-    exit
-fi
+set -xe;
+for i in {0..9}; do 
+    # use a tmp dir to avoid overwriting the original files in case re-run
+    tgt=${TMP_DIR}/${pattern}_${i}_tree.root;
+    python3 ${SCRIPT_DIR}/haddnano.py ${tgt} ${path}/${pattern}_more/${pattern}_${i}_tree.root; 
+done
+
+mv ${TMP_DIR}/*.root ${output_path};
