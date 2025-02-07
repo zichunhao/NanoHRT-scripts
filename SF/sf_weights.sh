d687c184cb9bd971566c7d01fe277b747ee257b4
zichunhao <zichunh2000@gmail.com>
2025-02-07 07:35:12 +0100

    Parallelize SF weights

--- SF/sf_weights.sh
+++ SF/sf_weights.sh
@@ -8,30 +8,37 @@ source "../TAG.sh"
 
 process_era() {
     local era=$1
+    local mode="$2"
     json_dir="${SCRIPT_DIR}/jsons/${era}"
 
-    # data
-    input_dir="${HRT_DIR}/${TAG}_ak8_qcd_${era}/data/outputs"
-    output_dir="${HRT_DIR}/${TAG}_ak8_qcd_${era}/data/outputs_sf_weights"
-    python3 sf_weights.py \
-    --input-dir $input_dir \
-    --signal-types $DATA_TYPES \
-    --is-data \
-    --json-dir $json_dir \
-    --output-dir $output_dir
-    
-    # mc
-    input_dir="${HRT_DIR}/${TAG}_ak8_qcd_${era}/mc/outputs"
-    output_dir="${HRT_DIR}/${TAG}_ak8_qcd_${era}/mc/outputs_sf_weights"
-    python3 sf_weights.py \
-    --input-dir $input_dir \
-    --signal-types $MC_TYPES \
-    --json-dir $json_dir \
-    --output-dir $output_dir
+    echo "Processing era: $era, mode: $mode"
+
+    if [ $mode == "data" ]; then
+        input_dir="${HRT_DIR}/${TAG}_ak8_qcd_${era}/data/outputs"
+        output_dir="${HRT_DIR}/${TAG}_ak8_qcd_${era}/data/outputs_sf_weights"
+        python3 sf_weights.py \
+        --is-data \
+        --input-dir $input_dir \
+        --signal-types $DATA_TYPES \
+        --json-dir $json_dir \
+        --output-dir $output_dir
+    else
+        # mc
+        input_dir="${HRT_DIR}/${TAG}_ak8_qcd_${era}/mc/outputs"
+        output_dir="${HRT_DIR}/${TAG}_ak8_qcd_${era}/mc/outputs_sf_weights"
+        python3 sf_weights.py \
+        --input-dir $input_dir \
+        --signal-types $MC_TYPES \
+        --json-dir $json_dir \
+        --output-dir $output_dir
+    fi
 }
 
-# for era in 2022 2022EE 2023 2023BPix; do
-# for era in 2022 2023BPix; do
-for era in 2022; do
-    process_era $era
+for era in 2022 2022EE 2023 2023BPix; do
+    for mode in "data" "mc"; do
+        process_era $era $mode &
+    done
 done
+
+wait
+echo "All done"
